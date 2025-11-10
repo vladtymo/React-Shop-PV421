c9f33bfc02ec21a757d7fcc1ba8b52c20cff3daa
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-11-10 18:29:29 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Layout from './components/Layout'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './components/Home'
-import ProductList from './components/ProductList'
-import NoPage from './components/NoPage'
-import ProductForm from './components/ProductForm'
-import LoginForm from './components/LoginForm'
-import LogoutPage from './components/LogoutPage'
+import "./App.css";
+import Layout from "./components/Layout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import ProductList from "./components/ProductList";
+import NoPage from "./components/NoPage";
+import ProductForm from "./components/ProductForm";
+import LoginForm from "./components/LoginForm";
+import LogoutPage from "./components/LogoutPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="products" element={<ProductList />} />
           <Route path="create" element={<ProductForm />} />
@@ -26,7 +23,7 @@ function App() {
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
